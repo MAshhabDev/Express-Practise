0669232c9c1575cab496122903dc8fd22f6370e9
Mahir4098 <mahirjr98@gmail.com>
2026-05-15 12:50:38 +0600

    Imlement Get api to get all user and also Implement Get Api with id to get user by id

--- src/server.ts
+++ src/server.ts
@@ -7,19 +7,21 @@ import { Pool } from "pg";
 const app: Application = express();
 const port = 5000;
 
-// This is a middleware for json data to show the data as json not undefined
+// This is a middleware for json data to read the json body and show the data as json not undefined
 app.use(express.json());
-// // This is a middleware for text data to show the data as json not undefined
+// // This is a middleware for text data to show the data as text not undefined
 
 app.use(express.text());
-// // This is a middleware for nested data to show the data as json not undefined
+// // This is a middleware for nested/form data to show the data as json not undefined
 
 app.use(express.urlencoded({ extended: true }));
 
+// Connect the PostGreSQL
 const pool = new Pool({
   connectionString:
 });
 
+// Create Db table user in the db
 const initDB = async () => {
   try {
     await pool.query(`
@@ -50,31 +52,88 @@ app.get("/", (req: Request, res: Response) => {
   });
 });
 
-app.post("/", async (req: Request, res: Response) => {
+// Create A post
+app.post("/api/users", async (req: Request, res: Response) => {
   //   console.log(req.body);
- try {
-     const { name, email, age, password } = req.body;
+  try {
+    const { name, email, age, password } = req.body;
 
-
-     //For insert data into table 
-  const result = await pool.query(
-    `
+    //For insert data into table
+    const result = await pool.query(
+      `
     INSERT INTO users (name, email,age,password) VALUES ($1,$2,$3,$4)
     RETURNING *
     `,
-    [name, email, age, password],
-  );
-  res.status(201).json({
-    message: "New Data Created",
-    data: result.rows[0],      //To get all data
-  });
- } catch (error:any) {
+      [name, email, age, password],
+    );
+    res.status(201).json({
+      message: "New Data Created",
+      data: result.rows[0], //To get all data
+    });
+  } catch (error: any) {
     res.status(500).json({
-    message: error.message,
-    error:error,
- })
-}
+      message: error.message,
+      error: error,
+    });
+  }
 });
+
+// Get all the post
+
+app.get("/api/users", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`
+    SELECT * FROM users`);
+    res.status(200).json({
+      success: true,
+      message: "Users Rettrived Successfully",
+      data: result.rows,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+
+      message: error.message,
+      error: error,
+    });
+  }
+});
+
+// Get all post by id
+
+app.get("/api/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query(
+      `
+  
+  SELECT * FROM users WHERE id=$1`,
+      [id],
+    );
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "Users did not find",
+        data: [],
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Users Per Id Rettrived Successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+
+      message: error.message,
+      error: error,
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
