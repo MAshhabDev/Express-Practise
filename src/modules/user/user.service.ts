75cb80739567f211a05ecfa496c2f101f4746a36
Mahir4098 <mahirjr98@gmail.com>
2026-05-17 13:44:42 +0600

    Implement Hashing Function BCRYPT to encrypt the password

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,23 +1,32 @@
 import { pool } from "../../db";
 import type { IUser } from "./user.interface";
 
+import bcrypt from "bcryptjs";
+
 const createUserIntoDB = async (payload: IUser) => {
   //For insert data into table
   const { name, email, age, password } = payload;
+
+  const hashPassword = await bcrypt.hash(password, 10);
 
   const result = await pool.query(
     `
     INSERT INTO users (name, email,age,password) VALUES ($1,$2,$3,$4)
     RETURNING *
     `,
-    [name, email, age, password],
+    [name, email, age, hashPassword],
   );
+  delete result.rows[0].password;
+
   return result;
 };
 
 const getUserFromDB = async () => {
   const result = await pool.query(`
     SELECT * FROM users`);
+
+  delete result.rows[0].password;
+
   return result;
 };
 
@@ -28,6 +37,7 @@ const getSingleUserFromDB = async (id: string) => {
   SELECT * FROM users WHERE id=$1`,
     [id],
   );
+  delete result.rows[0].password;
 
   return result;
 };
@@ -35,14 +45,23 @@ const getSingleUserFromDB = async (id: string) => {
 const updateUserFromDB = async (payload: IUser, id: string) => {
   const { name, age, password } = payload;
 
+  let hashPassword = password;
+
+  // Only when user update the password then it will go in this function
+  if (password) {
+    hashPassword = await bcrypt.hash(password, 10);
+  }
+
   const result = await pool.query(
     `UPDATE users SET 
       name= COALESCE ($1,name), 
       age=COALESCE ($2,age), 
       password=COALESCE ($3,password)
       WHERE id=$4 RETURNING *`,
-    [name, age, password, id],
+    [name, age, hashPassword, id],
   );
+  delete result.rows[0].password; //For delete the password row to show the user
+
   return result;
 };
 
